0264f17a061ce62724629ab219948dada668b551
Georgiy Odisharia <math.kraut.cat@gmail.com>
2018-01-25 11:47:14 +0300

    Stub implementation of file backend for logger is done.
     • Added defaults methods for logging
     • Changed mode (binmode and open mode) handlers
     • Get rid of Log::Any::Plugin
     • Now for file logging app's adapter is used
    
    Signed-off-by: Georgiy Odisharia <math.kraut.cat@gmail.com>

--- lib/App/FatPacker/Script/Log/Adapter/File.pm
+++ lib/App/FatPacker/Script/Log/Adapter/File.pm
@@ -53,16 +53,51 @@ sub init {
         $log_level = Log::Any::Adapter::Util::numeric_level('warning');
     }
     
+    my $mode = exists $self->{mode} and $self->{mode} ne '' ? $self->{mode} : '>';
+    if ($open_modes{$mode} and $mode ne '<') {
+        $self->{mode} = $open_modes{$mode};
+    } elsif (exists $open_modes_aliases{$mode} and $open_modes_aliases{$mode} ne '<') {
+        $self->{mode} = $open_modes_aliases{$mode};
+    } else {
+        $self->{mode} = '>';
+    }
+    
     my $filename = $self->{file};
-    my $mode = exists $open_modes{$self->{mode}} and $self->{mode}
-        ? $open_modes{$self->{mode}}
-        : 'r'
     $self->{file} = IO::File->new($filename, $mode);
     Carp::croak "Cannot open $filename for logging" unless defined $self->{file};
     
-    my $binmode = substr($self->{binmode}, 0, 1) eq ':'
-        ? $self->{binmode}
-        : ":$self->{binmode}";
+    my $binmode = exists $bin_modes{$self->{binmode}}
+        ? substr($self->{binmode}, 0, 1) eq ':'
+            ? $self->{binmode}
+            : ":" . $self->{binmode}
+        : ":utf8";
     $self->{file}->binmode($binmode);
     $self->{file}->autoflush(1);
+
+    # File locking possibility
+    $self->{__has_flock} = $Config{d_flock} || $Config{d_fcntl_can_lock} || $Config{d_lockf};
 }
+
+# Used from Log::Any::Adapter::File
+foreach my $method ( Log::Any::Adapter::Util::logging_methods() ) {
+    no strict 'refs';
+    my $method_level = Log::Any::Adapter::Util::numeric_level( $method );
+    *{$method} = sub {
+        my ( $self, $text ) = @_;
+        return if $method_level > $self->{log_level};
+        my $msg = sprintf( "[%s] %s\n", scalar(localtime), $text );
+        flock($self->{fh}, LOCK_EX) if $self->{__has_flock};
+        $self->{fh}->print($msg);
+        flock($self->{fh}, LOCK_UN) if $self->{__has_flock};
+      }
+}
+
+foreach my $method ( Log::Any::Adapter::Util::detection_methods() ) {
+    no strict 'refs';
+    my $base = substr($method,3);
+    my $method_level = Log::Any::Adapter::Util::numeric_level( $base );
+    *{$method} = sub {
+        return !!(  $method_level <= $_[0]->{log_level} );
+    };
+}
+
